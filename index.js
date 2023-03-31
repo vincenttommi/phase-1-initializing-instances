39e9faca33a5ebfb1ec277d5de2fd9b735f4d00c
vincenttommi <vincenttommi@gmail.com>
2023-03-31 16:40:22 +0300

    classes

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
-// Write your code here
+// // Write your code here
+
+
+// class Dog{
+
+
+//     constructor(name, breed){
+//      this.name = name;
+//      this.breed = breed;
+
+
+//     }
+// }
+
+
+//   //passing arguments in out constructor
+// let bigFluffyDog1 = new Dog("Buzz", "greatPyrenees");
+// let bigFluffyDog2 = new Dog("Woody", "labrador");
+
+
+ 
+
+
+
+//  console.log(bigFluffyDog1);
+//  console.log( bigFluffyDog2);
+
+// /
+
+
+class Breakfast{
+
+  constructor(food,drink){
+
+    this.food = food;
+    //defines its properties
+    this.drink = drink;
+
+
+
+
+  }
+}
+
+class Lunch{
+
+   
+    constructor(salad,soup,drink){
+
+
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+
+
+
+
+    }
+}
+
+
+class Dinner{
+
+
+ #dessert;
+ constructor(salad, soup, entree){
+
+ this.#dessert = this.#dessert;
+ this.salad = salad;
+ this.soup = soup;
+ this.entree  = entree;
+
+ }
+}
